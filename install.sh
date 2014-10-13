6604de1646ab39c7b9c9dcaa7129339b3b4119ab
vladdancer <vlad.dancer@gmail.com>
2014-10-13 16:01:02 +0300

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+#
+#
+
+# when this script runs first on the target host, update default package and install ansible to provision
+if [ ! -x /usr/bin/ansible ]; then
+  apt-get update
+  apt-get dist-upgrade
+  apt-get install git -y
+  apt-get install ansible python-apt -y
+fi
